5149fb7fd0e88422b28ea16bf16daf55e98981c1
Sibibalasankar <sibibalasankar1915@gmail.com>
2026-02-04 18:24:23 +0530

    Day 1 Code Updtaed

---
+++ main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        Student s1 = new Student(sc);
+    }
+}
