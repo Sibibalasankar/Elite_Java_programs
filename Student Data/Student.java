dbab51c07c6d0a9768c65ec46cceed9994f33d87
Sibibalasankar <sibibalasankar1915@gmail.com>
2026-02-06 19:51:18 +0530

    Search Option completed

---
+++ Student Data/Student.java
@@ -0,0 +1,131 @@
+import java.util.*;
+
+public class Student {
+    private String name;
+    private int regno;
+    private String rollno;
+    private int year;
+    private String dept;
+    private double cgpa;
+    private int m1, m2, m3;
+    private char sec;
+
+    Student(String d) {
+        AddEntry(d);
+        createRollno();
+        calculatecgpa(m1, m2, m3);
+    }
+
+    private void AddEntry(String d) {
+        String[] data = new String[5];
+        data = d.split(",");
+        name = data[0];
+        regno = Integer.parseInt(data[1]);
+        year = Integer.parseInt(data[2]);
+        dept = data[3];
+        sec = data[4].charAt(0);
+        m1=Integer.parseInt(data[5]);
+        m2=Integer.parseInt(data[6]);
+        m3=Integer.parseInt(data[7]);
+
+    }
+
+    private void createRollno() {
+        int a = year - 4;
+        String y = Integer.toString(a);
+        rollno = y.substring(2, 4) + dept.substring(0, 2) + sec + validatereg(regno);
+    }
+
+    private String validatereg(int reg) {
+        if (reg > 0 && reg < 9) {
+            return "0" + Integer.toString(reg);
+        } else {
+            return Integer.toString(reg);
+        }
+    }
+
+    void displaydetails() {
+        System.out.println("-----Student Details-----");
+        System.out.println("Name: " + name);
+        System.out.println("Reg No: " + regno);
+        System.out.println("Year: " + year);
+        System.out.println("Department: " + dept);
+        System.out.println("Section: " + sec);
+        System.out.println("Roll No: " + rollno);
+        System.out.println("CGPA: " + cgpa);
+    }
+
+    String getrollno() {
+        return rollno;
+    }
+
+    private void calculatecgpa(int m1, int m2, int m3) {
+        cgpa = m1 + m2 + m3 /3.0 ;
+    }
+       
+    static void searchStudent(Scanner sc, Student[] s, int count, int option) {
+
+    boolean found = false;
+
+    switch (option) {
+
+        case 1: 
+            System.out.println("Enter Year:");
+            int year = sc.nextInt();
+
+            for (int i = 0; i < count; i++) {
+                if (s[i].year == year) {
+                    s[i].displaydetails();
+                    found = true;
+                }
+            }
+            break;
+
+        case 2: 
+            System.out.println("Enter Roll No:");
+            String roll = sc.next();
+
+            for (int i = 0; i < count; i++) {
+                if (s[i].rollno.equals(roll)) {
+                    s[i].displaydetails();
+                    found = true;
+                    break;
+                }
+            }
+            break;
+
+        case 3: 
+            System.out.println("Enter Department:");
+            String dept = sc.next();
+
+            for (int i = 0; i < count; i++) {
+                if (s[i].dept.equalsIgnoreCase(dept)) {
+                    s[i].displaydetails();
+                    found = true;
+                }
+            }
+            break;
+
+        case 4: 
+            System.out.println("Enter Section:");
+            char sec = sc.next().charAt(0);
+
+            for (int i = 0; i < count; i++) {
+                if (s[i].sec == sec) {
+                    s[i].displaydetails();
+                    found = true;
+                }
+            }
+            break;
+
+        default:
+            System.out.println("Invalid Search Option");
+            return;
+    }
+
+    if (!found) {
+        System.out.println("No matching student found.");
+    }
+}
+ 
+}
